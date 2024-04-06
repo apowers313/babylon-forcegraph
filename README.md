edeaa538bc784725343507a3f6d32e5a7c5f445d
Adam Powers <apowers@ato.ms>
2024-04-06 16:48:53 -0700

    fix online viewing

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-Progressive XR Forcegraph with batteries included.
+[Progressive WebXR](https://blog.mozvr.com/progressive-webxr-ar-store/)
+Forcegraph with batteries included. Inspired by [three-forcegraph](https://github.com/vasturiano/three-forcegraph).
 
 Examples:
-- Basic (source)
-- Pretty (source)
+- [Basic](https://apowers313.github.io/babylon-forcegraph/examples/basic.html) ([source](https://github.com/apowers313/babylon-forcegraph/blob/master/examples/basic.html))
 - Styled (source)
+- Pretty (source)
 - Loading JSON data (source)
 - Expanding (source)
+- D3 Graph Engine (source)
+- Large Graph (source)
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,44 @@
-import { defineConfig, loadEnv } from 'vite'
-import fs from 'fs'
+import { defineConfig, loadEnv } from "vite";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [],
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: "./index.ts",
+        name: "HtmlMesh",
+        // the proper extensions will be added
+        fileName: "babylon-forcegraph",
+      },
+      rollupOptions: {
+        external: [
+          '@babylonjs/core',
+          '@babylonjs/inspector',
+          '@babylonjs/loaders'
+        ],
+        output: {
+          globals: {
+            '@babylonjs/core': 'BABYLON',
+            '@babylonjs/inspector': 'BABYLON',
+            '@babylonjs/loaders': 'BABYLON'
+          }
+        }
+      }
+    },
+    optimizeDeps: {
+      exclude: [
+        "@babylonjs/core",
+        "@babylonjs/inspector",
+        "@babylonjs/loaders"
+      ]
+    },
     server: {
       hmr: {
         host: env.HOST
@@ -18,10 +49,15 @@ export default defineConfig(({ command, mode }) => {
       },
       port: parseInt(env.PORT),
       https: {
-        key: fs.readFileSync(env.HTTPS_KEY_PATH),
-        cert: fs.readFileSync(env.HTTPS_CERT_PATH),
+        key: readFileSync(env.HTTPS_KEY_PATH),
+        cert: readFileSync(env.HTTPS_CERT_PATH),
       }
-    }
+    },
+    resolve: {
+      alias: {
+        "babylon-forcegraph": resolve(__dirname, "./index.ts"),
+      },
+    },
   }
 })
 
