dddeb97be1a5c0c35345da53624298f317290eac
Adam Powers <apowers@ato.ms>
2024-04-20 11:54:10 -0700

    add webxr buttons (wip)

--- src/Graph.ts
+++ src/Graph.ts
@@ -7,9 +7,10 @@ import {
     PhotoDome,
     Scene,
     Vector3,
+    WebXREnterExitUIButton,
 } from "@babylonjs/core";
 import {EdgeEvent, EventCallbackType, EventType, GraphEvent, NodeEvent} from "./Events";
-import {FetchEdges, FetchNodes, GraphConfig, GraphOpts, getConfig, getJsonDataOpts} from "./Config";
+import {FetchEdges, FetchNodes, GraphConfig, GraphOpts, LoadJsonDataOpts, getConfig, getJsonDataOpts} from "./Config";
 import {GraphEngine, GraphEngineNames} from "./engine/GraphEngine";
 import {Node, NodeIdType} from "./Node";
 import {D3GraphEngine} from "./engine/D3GraphEngine";
@@ -129,11 +130,43 @@ export class Graph {
             this.scene.render();
         });
 
+        // add enter vr / ar buttons
+        addCss();
+        const overlay = addButtonOverlay(this);
+        const buttonsArray = [
+            mkButton("VR", "immersive-vr", "unbounded"),
+            mkButton("AR", "immersive-ar", "unbounded"),
+        ];
+
+        for (const btn of buttonsArray) {
+            console.log("adding btn");
+            overlay.appendChild(btn.element);
+        }
+
         // WebXR setup
         if (navigator.xr) {
             await this.scene.createDefaultXRExperienceAsync({
+                // uiOptions: {
+                //     customButtons: buttonsArray,
+                // },
                 disableTeleportation: true,
+                // optionalFeatures: true,
+                // outputCanvasOptions: {
+                //     canvasOptions: {
+                //         framebufferScaleFactor: 0.5,
+                //     },
+                // },
             });
+        } else {
+            // create html button
+            const noXrBtn = document.createElement("button");
+            noXrBtn.classList.add("webxr-button");
+            noXrBtn.classList.add("webxr-not-available");
+            noXrBtn.innerHTML = "VR / AR NOT AVAILABLE";
+            overlay.appendChild(noXrBtn);
+            setTimeout(() => {
+                noXrBtn.remove();
+            }, 5000);
         }
 
         // Watch for browser/canvas resize events
@@ -257,11 +290,85 @@ export class Graph {
     }
 }
 
-interface LoadJsonDataOpts {
-    nodeListProp?: string;
-    edgeListProp?: string;
-    nodeIdProp?: string;
-    edgeSrcIdProp?: string;
-    edgeDstIdProp?: string;
-    fetchOpts?: Parameters<typeof fetch>[1];
+function mkButton(text: string, sessionMode?: XRSessionMode, referenceSpaceType?: XRReferenceSpaceType): WebXREnterExitUIButton {
+    sessionMode = sessionMode || "immersive-vr";
+    referenceSpaceType = referenceSpaceType || "local-floor";
+
+    // create html button
+    const hmdBtn = document.createElement("button");
+    // hmdBtn.className = "babylonVRicon";
+    hmdBtn.classList.add("webxr-button");
+    hmdBtn.classList.add("webxr-available");
+    // hmdBtn.title = `${sessionMode} - ${referenceSpaceType}`;
+    hmdBtn.innerHTML = text;
+
+    // create babylon button
+    const ret = new WebXREnterExitUIButton(hmdBtn, sessionMode, referenceSpaceType);
+    ret.update = function(activeButton: WebXREnterExitUIButton) {
+        this.element.style.display = activeButton === null || activeButton === this ? "" : "none";
+        // TODO: update for new CSS classes
+        hmdBtn.className = `babylonVRicon${activeButton === this ? " vrdisplaypresenting" : ""}`;
+    };
+
+    return ret;
+}
+
+function addCss() {
+    const css = `
+    .webxr-button {
+        font-family: 'Verdana', sans-serif;
+        font-size: 1em;
+        font-weight: bold;
+        color: white;
+        border: 2px solid white;
+        padding: 4px 16px 4px 16px;
+        margin-left: 10px;
+        border-radius: 8px;
+    }
+
+    .webxr-available {
+        background: black;
+        box-shadow:0 0 0 0px white, 0 0 0 2px black;
+    }
+
+    .webxr-not-available {
+        background: grey;
+        box-shadow:0 0 0 0px white, 0 0 0 2px grey;
+    }
+
+    .webxr-available:hover {
+        transform: scale(1.05);
+    } 
+
+    .webxr-available:active {
+        background-color: rgba(51,51,51,1);
+    } 
+    
+    .webxr-available:focus {
+        background-color: rgba(51,51,51,1);
+    }`;
+    // const url =
+    // typeof SVGSVGElement === "undefined" ?
+    //     "https://cdn.babylonjs.com/Assets/vrButton.png" :
+    //     "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%222048%22%20height%3D%221152%22%20viewBox%3D%220%200%202048%201152%22%20version%3D%221.1%22%3E%3Cpath%20transform%3D%22rotate%28180%201024%2C576.0000000000001%29%22%20d%3D%22m1109%2C896q17%2C0%2030%2C-12t13%2C-30t-12.5%2C-30.5t-30.5%2C-12.5l-170%2C0q-18%2C0%20-30.5%2C12.5t-12.5%2C30.5t13%2C30t30%2C12l170%2C0zm-85%2C256q59%2C0%20132.5%2C-1.5t154.5%2C-5.5t164.5%2C-11.5t163%2C-20t150%2C-30t124.5%2C-41.5q23%2C-11%2042%2C-24t38%2C-30q27%2C-25%2041%2C-61.5t14%2C-72.5l0%2C-257q0%2C-123%20-47%2C-232t-128%2C-190t-190%2C-128t-232%2C-47l-81%2C0q-37%2C0%20-68.5%2C14t-60.5%2C34.5t-55.5%2C45t-53%2C45t-53%2C34.5t-55.5%2C14t-55.5%2C-14t-53%2C-34.5t-53%2C-45t-55.5%2C-45t-60.5%2C-34.5t-68.5%2C-14l-81%2C0q-123%2C0%20-232%2C47t-190%2C128t-128%2C190t-47%2C232l0%2C257q0%2C68%2038%2C115t97%2C73q54%2C24%20124.5%2C41.5t150%2C30t163%2C20t164.5%2C11.5t154.5%2C5.5t132.5%2C1.5zm939%2C-298q0%2C39%20-24.5%2C67t-58.5%2C42q-54%2C23%20-122%2C39.5t-143.5%2C28t-155.5%2C19t-157%2C11t-148.5%2C5t-129.5%2C1.5q-59%2C0%20-130%2C-1.5t-148%2C-5t-157%2C-11t-155.5%2C-19t-143.5%2C-28t-122%2C-39.5q-34%2C-14%20-58.5%2C-42t-24.5%2C-67l0%2C-257q0%2C-106%2040.5%2C-199t110%2C-162.5t162.5%2C-109.5t199%2C-40l81%2C0q27%2C0%2052%2C14t50%2C34.5t51%2C44.5t55.5%2C44.5t63.5%2C34.5t74%2C14t74%2C-14t63.5%2C-34.5t55.5%2C-44.5t51%2C-44.5t50%2C-34.5t52%2C-14l14%2C0q37%2C0%2070%2C0.5t64.5%2C4.5t63.5%2C12t68%2C23q71%2C30%20128.5%2C78.5t98.5%2C110t63.5%2C133.5t22.5%2C149l0%2C257z%22%20fill%3D%22white%22%20/%3E%3C/svg%3E%0A";
+    // let css =
+    // `.babylonVRicon { color: #868686; border-color: #868686; border-style: solid; margin-left: 10px; height: 50px; width: 80px; background-color: rgba(51,51,51,0.7); background-image: url(${
+    //     url
+    // }); background-size: 80%; background-repeat:no-repeat; background-position: center; border: none; outline: none; transition: transform 0.125s ease-out } .babylonVRicon:hover { transform: scale(1.05) } .babylonVRicon:active {background-color: rgba(51,51,51,1) } .babylonVRicon:focus {background-color: rgba(51,51,51,1) }`;
+    // css += ".babylonVRicon.vrdisplaypresenting { background-image: none;} .vrdisplaypresenting::after { content: \"EXIT\"} .xr-error::after { content: \"ERROR\"}";
+    const style = document.createElement("style");
+    style.appendChild(document.createTextNode(css));
+    document.getElementsByTagName("head")[0].appendChild(style);
+}
+
+function addButtonOverlay(g: Graph): HTMLElement {
+    const overlay = document.createElement("div");
+    overlay.style.cssText = "z-index:11;position: absolute; right: 20px;bottom: 50px;";
+    overlay.classList.add("xr-button-overlay");
+    const renderCanvas = g.scene.getEngine().getInputElement();
+    if (renderCanvas && renderCanvas.parentNode) {
+        renderCanvas.parentNode.appendChild(overlay);
+    }
+
+    return overlay;
 }
